81b22ba3b6d1d89b03d6a95e8fbf4ee4525f1d9c
eyahiya-dev <eyahiya.dev@gmail.com>
2023-02-10 03:11:46 +0600

    Try to solving Basic 15  Problem on FreeCodeCamp in Js

---
+++ Day 01/problem 01 .js
@@ -0,0 +1,114 @@
+// 1. Try creating one of each type of comment.
+
+// This is single line comment
+
+/*
+    There are two type of comment in Programming.
+    single line comment and multi line comment.
+*/
+
+// 2. Use the var keyword to create a variable called myName.
+
+var myName;
+
+// 3. Assign the value 7 to variable a.
+
+var a;
+a = 7;
+
+// 4. Assign the contents of a to variable b.
+
+var a;
+a = 7;
+var b;
+b = a;
+
+// 5. Define a variable a with var and initialize it to a value of 9.
+
+var a = 9;
+
+// 6. Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+
+var myFirstName = "Md";
+var myLastName = "Eyahiya";
+
+// 7. Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined.
+// a would be 6 and be would be 15 and c would be "I am a String!".
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+/*
+    8. Modify the existing declarations and assignments so their names use camelCase.
+
+Do not create any new variables.
+
+*/
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// 9. Update the code so it only uses the let keyword.
+/*
+var catName = "Oliver";
+var catSound = "Meow!";
+*/
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+/* 10. Change the code so that all variables are declared using let or const. Use let when you want the variable to change, and const when you want the variable to remain constant. Also, rename variables declared with const to conform to common practices. Do not change the strings assigned to the variables.
+
+var fCC = "freeCodeCamp"; // Change this line
+var fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(fCC, fact); // Change this line
+
+*/
+
+const FCC = "freeCodeCamp";
+let fact = "is cool!";
+fact = "is awesome!";
+console.log(FCC, fact);
+
+// 11. Change the 0 so that sum will equal 20.
+// const sum = 10 + 0;
+
+const sum = 10 + 10;
+
+// 12. Change the 0 so the difference is 12.
+// const difference = 45 - 0;
+
+const difference = 45 - 33;
+
+// 13. Change the 0 so that product will equal 80.
+// const product = 8 * 0;
+
+const product = 8 * 10;
+
+// 14. Change the 0 so that the quotient is equal to 2.
+// const quotient = 66 / 0;
+
+const quotient = 66 / 33;
+
+// 15. Change the code to use the ++ operator on myVar.
+// let myVar = 87;
+
+let myVar = 87;
+myVar++;
+
+
+
+// note: i am successfully solved 15 question and my score is 13% on FreeCodeCamp.
--- Problem 01/problem 01 .js
+++
@@ -1,8 +0,0 @@
-// Try creating one of each type of comment.
-
-// This is single line comment
-
-/*
-    There are two type of comment in Programming.
-    single line comment and multi line comment.
-*/
