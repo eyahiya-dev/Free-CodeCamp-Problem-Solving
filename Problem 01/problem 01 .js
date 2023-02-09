2deb75460e81c3fe4059dcccf251e4b6cb906cd7
eyahiya-dev <eyahiya.dev@gmail.com>
2023-02-10 02:13:27 +0600

    Try to solve Free CodeCamp Problem in JS

---
+++ Problem 01/problem 01 .js
@@ -0,0 +1,8 @@
+// Try creating one of each type of comment.
+
+// This is single line comment
+
+/*
+    There are two type of comment in Programming.
+    single line comment and multi line comment.
+*/
