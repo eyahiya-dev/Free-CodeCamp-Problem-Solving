47e51851951b43853524ea0faa181015998cd767
eyahiya-dev <eyahiya.dev@gmail.com>
2023-02-18 20:00:06 +0600

    Solving Problem form FreeCodeCamp

---
+++ Day 02/dayTwo.js
@@ -0,0 +1,104 @@
+// let myVar = 87;
+// Change the code to use the ++ operator on myVar.
+let myVar = 87;
+console.log(++myVar);
+
+// let myVar = 11;
+// Change the code to use the -- operator on myVar.
+let myVar2 = 11;
+console.log(--myVar2);
+
+// Create a variable myDecimal and give it a decimal value with a fractional part (e.g. 5.7).
+const myDecimal = 10.5;
+
+// Change the 0.0 so that product will equal 5.0.
+// const product = 2.0 * 0.0;
+const product = 2.0 * 2.5;
+console.log(product);
+
+// Change the 0.0 so that quotient will equal to 2.2.
+// const quotient = 0.0 / 2.0; // Change this line
+
+const quotient = 4.4 / 2.0;
+console.log(quotient);
+
+// Set remainder equal to the remainder of 11 divided by 3 using the remainder (%) operator.
+const remainder = 11 % 3;
+console.log(remainder);
+
+// Convert the assignments for a, b, and c to use the += operator.
+let a = 3;
+let b = 17;
+let c = 12;
+
+a += 12;
+b += 9;
+c += 7;
+
+// Convert the assignments for x, y, and z to use the -= operator.
+let x = 11;
+let y = 9;
+let z = 3;
+
+x -= 6;
+y -= 15;
+z -= 1;
+
+//Convert the assignments for a, b, and c to use the *= operator.
+let m = 5;
+let n = 12;
+let o = 4.6;
+
+m *= 5;
+n *= 3;
+o *= 10;
+
+// Convert the assignments for a, b, and c to use the /= operator.
+let q = 48;
+let r = 108;
+let s = 33;
+
+q /= 12;
+r /= 4;
+s /= 11;
+
+// Use backslashes to assign a string to the myStr variable so that if you were to print it to the console, you would see:
+const myStr = 'I am a "double quoted" string inside "double quotes".';
+
+/*
+Change the provided string to a string with single quotes at the beginning and end and no escape characters.
+
+Right now, the <a> tag in the string uses double quotes everywhere. You will need to change the outer quotes to single quotes so you can remove the escape characters.
+*/
+// const myStr2 = "<a href=\"http://www.example.com\" target=\"_blank\">Link</a>";
+const myStr2 = '<a href="http://www.example.com" target="_blank">Link</a>'; //Correct form.
+
+/*
+Code	    Output
+\'	    single quote
+\"	    double quote
+\\	    backslash
+\n	    newline
+\t	    tab
+\r	    carriage return
+\b	    word boundary
+\f	    form feed
+
+*/
+
+const newLine = "FirstLine\n\t\\SecondLine\nThirdLine\bFourthLine";
+
+// Build myStr from the strings This is the start. and This is the end. using the + operator. Be sure to include a space between the two strings.
+const myStr3 = "This is the start. " + "This is the end";
+
+// Build myStr over several lines by concatenating these two strings: This is the first sentence. and This is the second sentence. using the += operator. Use the += operator similar to how it is shown in the example and be sure to include a space between the two strings. Start by assigning the first string to myStr, then add on the second string.
+
+let myStr4 = "This is the first sentence. ";
+myStr4 += "This is the second sentence.";
+
+// Set myName to a string equal to your name and build myStr with myName between the strings My name is and and I am well!
+
+const myName = "Yahya Mahmud";
+const profile = "My name is " + myName + "and I am well!";
+
+// 
---
+++ Day 03/dayThree.js
@@ -0,0 +1,107 @@
+// Set someAdjective to a string of at least 3 characters and append it to myStr using the += operator.
+const someAdjective = "developer";
+let myStr = "I am a part time ";
+myStr += someAdjective;
+
+// Use the .length property to set lastNameLength to the number of characters in lastName.
+let lastNameLength = 0;
+const lastName = "Lovelace";
+lastNameLength = lastName.length;
+
+//Use bracket notation to find the first character in the lastName variable and assign it to firstLetterOfLastName.
+let firstLetterOfLastName = "";
+const lastName1 = "Lovelace";
+console.log((firstLetterOfLastName = lastName1[0]));
+console.log((firstLetterOfLastName = lastName1[1]));
+console.log((firstLetterOfLastName = lastName1[2]));
+console.log((firstLetterOfLastName = lastName1[3]));
+
+// Correct the assignment to myStr so it contains the string value of Hello World using the approach shown in the example above.
+// let myStr = "Bob";
+// myStr[0] = "J";
+
+let myStr3 = "Bob";
+myStr3[0] = "J"; //This is not work
+myStr3 = "Job";
+console.log(myStr3);
+
+//Let's try to set thirdLetterOfLastName to equal the third letter of the lastName variable using bracket notation.
+const lastName2 = "Lovelace";
+const thirdLetterOfLastName = lastName[2];
+
+// Use bracket notation to find the last character in the lastName variable.
+const lastName3 = "Yahya Mahmud";
+const lastLetterof = lastName3[lastName3.length - 1];
+
+//Use bracket notation to find the second-to-last character in the lastName string.
+// const lastName4 = "Lovelace";
+const lastName4 = "Lovelace";
+const secondToLastLetterOfLastName = lastName4[lastName4.length - 3];
+
+/*
+In this challenge, we provide you with a noun, a verb, an adjective and an adverb. You need to form a complete sentence using words of your choice, along with the words we provide.
+
+[dog, big, run , quickly]
+
+You will need to use the string concatenation operator + to build a new string, using the provided variables: myNoun, myAdjective, myVerb, and myAdverb. You will then assign the formed string to the wordBlanks variable. You should not change the words assigned to the variables.
+
+You will also need to account for spaces in your string, so that the final sentence has spaces between all the words. The result should be a complete sentence.
+
+*/
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "run";
+const myAdverb = "quickly";
+
+const blankWord = myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb;
+console.log(blankWord);
+
+//Modify the new array myArray so that it contains both a string and a number (in that order).
+const myArray = ["Yahya", 24, "Student", "Web Developer"];
+
+//Create a nested array called myArray.
+const myArray1 = [
+  ["Yahya", 24],
+  ["Jahid", 24],
+];
+
+//Create a variable called myData and set it to equal the first value of myArray using bracket notation.
+const myArray3 = [50, 60, 70];
+const myData = myArray[0];
+
+//Modify the data stored at index 0 of myArray to a value of 45.
+const myArray4 = [18, 64, 99];
+myArray4[0] = 45;
+
+//Using bracket notation select an element from myArray such that myData is equal to 8.
+const myArray5 = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+
+const myData2 = myArray5[2][1];
+console.log(myData2);
+
+//Push ["dog", 3] onto the end of the myArray variable.
+const myArray6 = [
+  ["John", 23],
+  ["cat", 2],
+];
+myArray6.push(["dog", 3]);
+
+// Use the .pop() function to remove the last item from myArray and assign the popped off value to a new variable, removedFromMyArray.
+const myArray7 = [
+  ["John", 23],
+  ["cat", 2],
+];
+const removedFromMyArray = myArray7.pop();
+console.log(removedFromMyArray);
+console.log(myArray7);
+
+// Use the .shift() function to remove the first item from myArray and assign the "shifted off" value to a new variable, removedFromMyArray.
+const myArray8 = [["John", 23], ["dog", 3]];
+const removedFromMyArray2 = myArray8.shift();
+
